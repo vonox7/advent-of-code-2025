c30cfa0ba12a62260dc89b974eaba17a37016412
Valentin Slawicek <valentin@slawicek.com>
2025-12-01 23:50:16 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,30 @@
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var zeroCounter = 0
+        var currentPos = 50
+        for (line in input) {
+            val offset = line.replace("L", "-").replace("R", "").toInt()
+            currentPos = (currentPos + offset).mod(100)
+            if (currentPos == 0) zeroCounter++
+        }
+        return zeroCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var zeroCounter = 0
+        var currentPos = 50
+        for (line in input) {
+            val offset = line.replace("L", "-").replace("R", "").toInt()
+            repeat (offset.absoluteValue) {
+                currentPos = (currentPos + offset.sign).mod(100)
+                if (currentPos == 0) zeroCounter++
+            }
+        }
+        return zeroCounter
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
