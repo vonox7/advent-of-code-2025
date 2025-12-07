f9c9ac57f5c78d0420ae3df119ce4e35848bb64d
Valentin Slawicek <valentin@slawicek.com>
2025-12-07 15:39:46 +0100

    Day7 part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -19,11 +19,25 @@ fun main() {
         return splits
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var beams = input.first().map { if (it == 'S') 1L else 0L }.toMutableList()
+        var beamCount = 1L
+        input.drop(1).forEach { line ->
+            val newBeams = beams.toMutableList()
+            line.forEachIndexed { pos, char ->
+                if (char == '^') {
+                    newBeams[pos] = 0L
+                    newBeams[pos - 1] += beams[pos]
+                    newBeams[pos + 1] += beams[pos]
+                    beamCount += beams[pos]
+                }
+            }
+            beams = newBeams
+        }
+        return beamCount
     }
 
     val input = readInput("Day07")
-    part1(input).println()
-    //part2(input).println()
+    //part1(input).println()
+    part2(input).println()
 }
